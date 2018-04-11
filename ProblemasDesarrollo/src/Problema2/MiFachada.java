e8ef5de0db8f10be81459aace20e57204a1545c7
krypth <krypth@krypth-N61PB-M2S>
2018-04-11 00:17:53 -0500

    prueba

---
+++ ProblemasDesarrollo/src/Problema2/MiFachada.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Problema2;
+
+/**
+ *
+ * @author krypth
+ */
+public class MiFachada {
+    public MiFachada () {
+		
+	}
+	
+	public void llamar (int monto, String cuenta, String operacion) {	
+	    
+	}
+}
